fb5c56aac39329f836ddb778a257d47aa85f7460
Markbmag <143971599+Markbmag@users.noreply.github.com>
2026-08-19 09:39:49 +0300

    m

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import favicon from './assets/favicon.png';
+
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = favicon;
+document.head.appendChild(link);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
