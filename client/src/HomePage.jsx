1afcbb525096ecc5a07ea5bf999f1864d34e4f3c
Markbmag <143971599+Markbmag@users.noreply.github.com>
2026-09-01 12:03:49 +0300

    ljh

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -17,7 +17,7 @@ import HoldsSgpPage from './assets/hold.png';
 import sgpManagementPreview from './assets/cp8.png';
 import DrrCp7DashboardPage from './assets/cp7drr.png'; 
 import cp7historyPreview from './assets/cp7hist.png'; 
-import DrrCp8DashboardPage from './assets/cp7drr.png'; 
+import DrrCp8DashboardPage from './assets/cp8drr.png'; 
 
 
 // ====== СТИЛИ ======
