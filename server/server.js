52aebd7d17c5af7c7f6665ad404a57e412e000ed
Markbmag <143971599+Markbmag@users.noreply.github.com>
2026-09-11 08:48:59 +0300

    n

--- server/server.js
+++ server/server.js
@@ -8075,7 +8075,7 @@ app.get('/api/brigade-report/data', async (req, res) => {
   }
 });
 
-// ================== БРИГАДНЫЙ ОТЧЁТ – ТОП ДЕФЕКТОВ БЕЗ ВЛАДЕЛЬЦА ==================
+
 // ================== БРИГАДНЫЙ ОТЧЁТ – ТОП ДЕФЕКТОВ БЕЗ ВЛАДЕЛЬЦА ==================
 app.get('/api/brigade-report/unassigned-defects', async (req, res) => {
   try {
